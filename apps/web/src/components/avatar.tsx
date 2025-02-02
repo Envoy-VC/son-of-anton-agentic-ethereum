60ec519b1c87883994031d0e768a9c0c1364bd3b
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-03 02:45:04 +0530

    chore: base avatar component

---
+++ apps/web/src/components/avatar.tsx
@@ -0,0 +1,108 @@
+import { useGLTF } from '@react-three/drei';
+import type { AvatarModel } from '~/types';
+
+import type { GroupProps } from '@react-three/fiber';
+import {
+  headTargets,
+  leftEyeTargets,
+  rightEyeTargets,
+  teethTargets,
+} from '~/data/avatar';
+
+export const Avatar = (props: GroupProps) => {
+  const { nodes, materials } = useGLTF('/models/avatar.glb') as AvatarModel;
+  return (
+    <group {...props} dispose={null}>
+      <group name='Scene'>
+        <group name='Armature' userData={{ name: 'Armature' }}>
+          <primitive object={nodes.Hips} />
+          <skinnedMesh
+            name='EyeLeft'
+            geometry={nodes.EyeLeft.geometry}
+            material={materials.Wolf3D_Eye}
+            skeleton={nodes.EyeLeft.skeleton}
+            morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+            morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+            userData={{
+              targetNames: leftEyeTargets,
+              name: 'EyeLeft',
+            }}
+          />
+          <skinnedMesh
+            name='EyeRight'
+            geometry={nodes.EyeRight.geometry}
+            material={materials.Wolf3D_Eye}
+            skeleton={nodes.EyeRight.skeleton}
+            morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+            morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+            userData={{
+              targetNames: rightEyeTargets,
+              name: 'EyeRight',
+            }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Head'
+            geometry={nodes.Wolf3D_Head.geometry}
+            material={materials.Wolf3D_Skin}
+            skeleton={nodes.Wolf3D_Head.skeleton}
+            morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+            morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+            userData={{
+              targetNames: headTargets,
+              name: 'Wolf3D_Head',
+            }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Teeth'
+            geometry={nodes.Wolf3D_Teeth.geometry}
+            material={materials.Wolf3D_Teeth}
+            skeleton={nodes.Wolf3D_Teeth.skeleton}
+            morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+            morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+            userData={{
+              targetNames: teethTargets,
+              name: 'Wolf3D_Teeth',
+            }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Hair'
+            geometry={nodes.Wolf3D_Hair.geometry}
+            material={materials.Wolf3D_Hair}
+            skeleton={nodes.Wolf3D_Hair.skeleton}
+            userData={{ name: 'Wolf3D_Hair' }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Body'
+            geometry={nodes.Wolf3D_Body.geometry}
+            material={materials.Wolf3D_Body}
+            skeleton={nodes.Wolf3D_Body.skeleton}
+            userData={{ name: 'Wolf3D_Body' }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Outfit_Bottom'
+            geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+            material={materials.Wolf3D_Outfit_Bottom}
+            skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+            userData={{ name: 'Wolf3D_Outfit_Bottom' }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Outfit_Footwear'
+            geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+            material={materials.Wolf3D_Outfit_Footwear}
+            skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+            userData={{ name: 'Wolf3D_Outfit_Footwear' }}
+          />
+          <skinnedMesh
+            name='Wolf3D_Outfit_Top'
+            geometry={nodes.Wolf3D_Outfit_Top.geometry}
+            material={materials.Wolf3D_Outfit_Top}
+            skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+            userData={{ name: 'Wolf3D_Outfit_Top' }}
+          />
+        </group>
+      </group>
+    </group>
+  );
+};
+
+useGLTF.preload('/models/avatar.glb');
---
+++ apps/web/src/types/index.ts
@@ -0,0 +1,27 @@
+import type * as THREE from 'three';
+import type { GLTF } from 'three-stdlib';
+
+export type AvatarModel = GLTF & {
+  nodes: {
+    EyeLeft: THREE.SkinnedMesh;
+    EyeRight: THREE.SkinnedMesh;
+    Wolf3D_Head: THREE.SkinnedMesh;
+    Wolf3D_Teeth: THREE.SkinnedMesh;
+    Wolf3D_Hair: THREE.SkinnedMesh;
+    Wolf3D_Body: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Bottom: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Footwear: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Top: THREE.SkinnedMesh;
+    Hips: THREE.Bone;
+  };
+  materials: {
+    Wolf3D_Eye: THREE.MeshStandardMaterial;
+    Wolf3D_Skin: THREE.MeshStandardMaterial;
+    Wolf3D_Teeth: THREE.MeshStandardMaterial;
+    Wolf3D_Hair: THREE.MeshStandardMaterial;
+    Wolf3D_Body: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Bottom: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Footwear: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Top: THREE.MeshStandardMaterial;
+  };
+};
