238a3dc72ab90355b8a1d335075208a86458e7e6
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-06 22:29:57 +0530

    chore: add homepage spline design

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,13 @@
+import Spline from '@splinetool/react-spline/next';
+
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div className='hide-scrollbar max-h-screen overflow-hidden'>
+      <div className='h-screen scale-[120%]'>
+        <Spline scene='https://prod.spline.design/vvdKBoe9cyJsEWtU/scene.splinecode' />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
