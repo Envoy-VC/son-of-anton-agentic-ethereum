df3fe7e30846fa19af1cd1fff9a30b0658e53e27
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-06 23:58:29 +0530

    chore: add hero section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Spline from '@splinetool/react-spline/next';
+import { Hero } from '~/components';
 
 const Home = () => {
   return (
     <div className='hide-scrollbar max-h-screen overflow-hidden'>
-      <div className='h-screen scale-[120%]'>
+      <div className='relative h-screen scale-[120%]'>
         <Spline scene='https://prod.spline.design/vvdKBoe9cyJsEWtU/scene.splinecode' />
+        <Hero />
       </div>
     </div>
   );
---
+++ apps/web/src/components/hero.tsx
@@ -0,0 +1,11 @@
+export const Hero = () => {
+  return (
+    <div className='absolute top-1/4 right-1/2 flex w-full max-w-xs translate-x-1/2 flex-col gap-4 px-2 sm:max-w-md lg:max-w-xl xl:max-w-4xl'>
+      <div className='flex flex-col flex-wrap text-wrap border text-center font-medium text-[1.75rem] sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl'>
+        Your digital accomplice
+        <br />
+        in a world of analog minds.
+      </div>
+    </div>
+  );
+};
