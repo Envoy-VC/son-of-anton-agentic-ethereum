34e60689fcee0919e87d43690a627b2278d54440
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-08 19:15:19 +0530

    chore: fix biome errors

--- apps/web/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -9,6 +9,8 @@ import { createTRPCContext } from '~/server/api/trpc';
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
+
+// biome-ignore lint/suspicious/useAwait: safe
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
     headers: req.headers,
