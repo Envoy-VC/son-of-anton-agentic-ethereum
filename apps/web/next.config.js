dbeea737983fd12a1d53ef8630bdf0ff320eeb94
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-09 22:08:53 +0530

    chore: update config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
+import { join } from 'node:path';
 import './src/env.js';
 
-import {} from 'node:fs/promises';
+import { symlink } from 'node:fs/promises';
+import { access } from 'node:fs/promises';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -24,35 +26,35 @@ const nextConfig = {
     }
 
     // Workaround for https://github.com/vercel/next.js/issues/25852
-    // config.plugins.push(
-    //   new (class {
-    //     apply(compiler) {
-    //       compiler.hooks.afterEmit.tapPromise(
-    //         'SymlinkWebpackPlugin',
-    //         async (compiler) => {
-    //           if (isServer) {
-    //             const from = join(compiler.options.output.path, '../static');
-    //             const to = join(compiler.options.output.path, 'static');
+    config.plugins.push(
+      new (class {
+        apply(compiler) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
 
-    //             try {
-    //               await access(from);
-    //             } catch (error) {
-    //               // Access check failed, need to create symlink
-    //               if (error.code === 'ENOENT') {
-    //                 await symlink(to, from, 'junction');
-    //               } else {
-    //                 console.log(
-    //                   `SymlinkWebpackPlugin: Unexpected failure.  symlink ${from} -> ${to}`
-    //                 );
-    //                 throw error;
-    //               }
-    //             }
-    //           }
-    //         }
-    //       );
-    //     }
-    //   })()
-    // );
+                try {
+                  await access(from);
+                } catch (error) {
+                  // Access check failed, need to create symlink
+                  if (error.code === 'ENOENT') {
+                    await symlink(to, from, 'junction');
+                  } else {
+                    console.log(
+                      `SymlinkWebpackPlugin: Unexpected failure.  symlink ${from} -> ${to}`
+                    );
+                    throw error;
+                  }
+                }
+              }
+            }
+          );
+        }
+      })()
+    );
 
     return config;
   },
